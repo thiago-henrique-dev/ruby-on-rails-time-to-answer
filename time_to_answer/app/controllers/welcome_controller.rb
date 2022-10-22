ed747611ea6a15ed872e9360e6b1b76df91e3c6a
thiagohenriquedev <thiagohenriquedev>
2022-10-22 10:19:38 -0300

    Creating Admin and Profile logins

--- time_to_answer/app/controllers/welcome_controller.rb
+++ time_to_answer/app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  http_basic_authenticate_with name: "thg", password: "123"
+  #http_basic_authenticate_with name: "thg", password: "123"
   def index
   end
 end
