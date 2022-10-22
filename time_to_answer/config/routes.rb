3df8485ce8dfd4161d27c2ee55adbe4cc3ad58ee
thiagohenriquedev <thiagohenriquedev>
2022-10-21 22:28:52 -0300

    Time To Answer - Starting an application and homepage

---
+++ time_to_answer/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'inicio', to: 'welcome#index'
+
+  root to: 'welcome#index'
+  #ROTA PRINCIPAL
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
