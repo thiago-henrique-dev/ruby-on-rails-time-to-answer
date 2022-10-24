388135569483ddcd667a4fea75d2be4ac7b3f26b
thiagohenriquedev <thiagohenriquedev>
2022-10-24 11:40:11 -0300

    Using Devise to secure backoffices

--- time_to_answer/config/routes.rb
+++ time_to_answer/config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   devise_for :admins
   get 'inicio', to: 'site/welcome#index'
 
-  root to: 'site/welcome#index'
+  root to: 'site/ welcome#index'
   #ROTA PRINCIPAL
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
