9926ba2b1806bf0c28f9823f9f782ad171f92e1d
thiagohenriquedev <thiagohenriquedev>
2022-10-31 15:39:03 -0300

    Creating the Controller for Subjects

--- time_to_answer/config/routes.rb
+++ time_to_answer/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :admins_backoffice do
     get 'welcome/index' #Dashboard
     resources :admins
+    resources :subjects
   end
   
   devise_for :admins
