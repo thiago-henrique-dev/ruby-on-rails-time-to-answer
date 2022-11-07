ce21cd480b5c6a93e9cf62250887f1f63577af56
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-11-07 18:04:22 -0300

    Checking the correct answer

--- time_to_answer/app/controllers/site/answer_controller.rb
+++ time_to_answer/app/controllers/site/answer_controller.rb
@@ -1,5 +1,5 @@
 class Site::AnswerController < SiteController
-    def Question
-        puts ">>>> #{params[:answer]}"
+    def question
+        @answer = Answer.find(params[:answer_id])   
     end
 end
