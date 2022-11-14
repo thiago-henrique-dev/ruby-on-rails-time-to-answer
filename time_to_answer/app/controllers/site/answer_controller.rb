74b75087878c5520e6ba87e3737a1e1f3fdf3f74
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-11-14 16:45:26 -0300

    Using Redis in Rails

--- time_to_answer/app/controllers/site/answer_controller.rb
+++ time_to_answer/app/controllers/site/answer_controller.rb
@@ -1,7 +1,11 @@
 class Site::AnswerController < SiteController
     def question
-        @answer = Answer.find(params[:answer_id])   
-        UserStatistic.set_statistic(@answer, current_user)
+        #@answer = Answer.find(params[:answer_id])   
+        redis_answer = Redis.cache.read(params[:answer_id]).split("@@")
+        @question_id = redi_answer.first
+        @correct = ActiveModel::Type::Boolean.new.cast(redis_answer.last) 
+
+        UserStatistic.set_statistic(@correct, current_user)
     end
 
 end
